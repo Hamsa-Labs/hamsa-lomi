49715c9a154e243bb63eb8fd7a06b09e05472816
eliasn <nebiyuelias1@gmail.com>
2021-06-25 22:12:07 +0300

    Add lefthook.yml

--- hamsa_lomi/lib/main.dart
+++ hamsa_lomi/lib/main.dart
@@ -1,6 +1,10 @@
-import 'package:firebase_core/firebase_core.dart';
+// Flutter imports:
 import 'package:flutter/material.dart';
 
+// Package imports:
+import 'package:firebase_core/firebase_core.dart';
+
+// Project imports:
 import 'app.dart';
 
 void main() async {
