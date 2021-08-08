e4ce01795e93a57deed6c68b9ee537d110aedf6e
eliasn <nebiyuelias1@gmail.com>
2021-08-08 11:02:12 +0300

    Fix lint errors

--- hamsa_lomi/lib/presentation/share_widgets/tabsbar_widget.dart
+++ hamsa_lomi/lib/presentation/share_widgets/tabsbar_widget.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'appbar_widget.dart';
 import '../constants/app_assets_constant.dart';
 
 class HamsaTabBar extends StatefulWidget {
@@ -8,14 +7,13 @@ class HamsaTabBar extends StatefulWidget {
   final String firstText;
   final String secondText;
 
-
-  const HamsaTabBar({
-  Key? key,
-  required this.tab1,
-  required this.tab2,
-  required this.firstText,
-  required this.secondText
-  }) : super(key: key);
+  const HamsaTabBar(
+      {Key? key,
+      required this.tab1,
+      required this.tab2,
+      required this.firstText,
+      required this.secondText})
+      : super(key: key);
 
   @override
   _HamsaTabBarState createState() => _HamsaTabBarState();
@@ -23,8 +21,7 @@ class HamsaTabBar extends StatefulWidget {
 
 class _HamsaTabBarState extends State<HamsaTabBar>
     with SingleTickerProviderStateMixin {
-
-   TabController? _tabController ;
+  TabController? _tabController;
 
   // @override
   // void initState() {
@@ -41,50 +38,46 @@ class _HamsaTabBarState extends State<HamsaTabBar>
   @override
   Widget build(BuildContext context) {
     return Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: Container(
-          height: 50,
-
-          decoration: BoxDecoration(
-            color: Color(0xffF7F7F7),
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(10),
-              topRight: Radius.circular(10),
-              bottomRight: Radius.circular(10),
-              bottomLeft: Radius.circular(10),
-            ),
+      padding: const EdgeInsets.all(10.0),
+      child: Container(
+        height: 50,
+        decoration: BoxDecoration(
+          color: Color(0xffF7F7F7),
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(10),
+            topRight: Radius.circular(10),
+            bottomRight: Radius.circular(10),
+            bottomLeft: Radius.circular(10),
           ),
-          child: Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: TabBar(
-              controller: _tabController,
-              indicator: BoxDecoration(
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(10),
-                  topRight: Radius.circular(10),
-                  bottomRight: Radius.circular(10),
-                  bottomLeft: Radius.circular(10),
-                ),
-                color: HamsaColors.primaryColor,
-              ),
-              labelColor: HamsaColors.lightBackground,
-              unselectedLabelColor: Color(0xffC7C7C7),
-              tabs: [
-                Tab(
-                  text: widget.firstText,
-                  // child: widget.tab1,
-                ),
-                Tab(
-                  text: widget.secondText,
-                  // child: widget.tab2,
-                ),
-              ],
-
-            ),
-          ),
-
-
         ),
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: TabBar(
+            controller: _tabController,
+            indicator: BoxDecoration(
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(10),
+                topRight: Radius.circular(10),
+                bottomRight: Radius.circular(10),
+                bottomLeft: Radius.circular(10),
+              ),
+              color: HamsaColors.primaryColor,
+            ),
+            labelColor: HamsaColors.lightBackground,
+            unselectedLabelColor: Color(0xffC7C7C7),
+            tabs: [
+              Tab(
+                text: widget.firstText,
+                // child: widget.tab1,
+              ),
+              Tab(
+                text: widget.secondText,
+                // child: widget.tab2,
+              ),
+            ],
+          ),
+        ),
+      ),
       // ),
     );
   }
