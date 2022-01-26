3e7b266aee0e4b4c59ed78668711210432070dcb
eliasn <nebiyuelias1@gmail.com>
2022-01-26 07:46:30 +0300

    Add StartFundraiserButton

--- hamsa_lomi/lib/presentation/home/page/home_page.dart
+++ hamsa_lomi/lib/presentation/home/page/home_page.dart
@@ -1,5 +1,6 @@
 // Flutter imports:
 import 'package:flutter/material.dart';
+import '../widgets/start_fundraiser_button.dart';
 import '../../core/hamsa_app_bar.dart';
 
 // Project imports:
@@ -18,7 +19,10 @@ class HomePage extends StatelessWidget {
         withLogo: false,
         appBarText: Text('Home'),
       ),
-      body: Text('Home Page'),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: StartFundraiserButton(),
+      ),
       bottomNavigationBar: CustomBottomNavBar(),
     );
   }
--- hamsa_lomi/lib/presentation/widgets/hamsa_rounded_button.dart
+++ hamsa_lomi/lib/presentation/widgets/hamsa_rounded_button.dart
@@ -4,18 +4,22 @@ import 'package:flutter/material.dart';
 class HamsaRoundedButton extends StatelessWidget {
   final String label;
   final VoidCallback? onPressed;
+  final ButtonStyle? buttonStyle;
 
   const HamsaRoundedButton(
-      {Key? key, required this.label, required this.onPressed})
+      {Key? key,
+      required this.label,
+      required this.onPressed,
+      this.buttonStyle})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
+      style: buttonStyle,
       onPressed: onPressed,
       child: Text(
         label,
-        style: Theme.of(context).textTheme.button,
       ),
     );
   }
