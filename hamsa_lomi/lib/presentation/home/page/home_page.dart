82c32f66efbd41280883f5f1ff22fdcb217a8d33
eliasn <nebiyuelias1@gmail.com>
2022-01-31 18:25:53 +0300

    Displaying a horizonal scrolling carousel

--- hamsa_lomi/lib/presentation/home/page/home_page.dart
+++ hamsa_lomi/lib/presentation/home/page/home_page.dart
@@ -1,9 +1,9 @@
 // Flutter imports:
 import 'package:flutter/material.dart';
+import '../widgets/horizontal_scroll_view.dart';
 
 // Project imports:
 import '../../core/widgets/hamsa_app_bar.dart';
-import '../../core/widgets/hamsa_campaign_card.dart';
 import '../widgets/custom_bottom_nav_bar.dart';
 import '../widgets/fundraising_filter.dart';
 import '../widgets/start_fundraiser_button.dart';
@@ -23,14 +23,24 @@ class HomePage extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            StartFundraiserButton(),
-            SizedBox(
-              height: 8.0,
+        child: CustomScrollView(
+          slivers: [
+            SliverToBoxAdapter(
+              child: StartFundraiserButton(),
             ),
-            FundraiserFilter(),
-            HamsaCampaignCard()
+            SliverPadding(
+              padding: EdgeInsets.all(8.0),
+            ),
+            SliverToBoxAdapter(
+              child: FundraiserFilter(),
+            ),
+            SliverToBoxAdapter(
+              child: HorizontalScrollView(label: 'Emergency Donations'),
+            ),
+            SliverPadding(padding: EdgeInsets.all(4.0)),
+            SliverToBoxAdapter(
+              child: HorizontalScrollView(label: 'Recent Fundraisers'),
+            )
           ],
         ),
       ),
