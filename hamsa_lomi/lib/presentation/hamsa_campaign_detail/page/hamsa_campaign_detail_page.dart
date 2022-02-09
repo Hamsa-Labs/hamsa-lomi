7fe0b82d89375f50714a9c18a51c9cbfff24c016
eliasn <nebiyuelias1@gmail.com>
2022-02-09 07:39:13 +0300

    Open share tray when share button is pressed

--- hamsa_lomi/lib/presentation/hamsa_campaign_detail/page/hamsa_campaign_detail_page.dart
+++ hamsa_lomi/lib/presentation/hamsa_campaign_detail/page/hamsa_campaign_detail_page.dart
@@ -1,9 +1,12 @@
+import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
 import 'package:flutter/material.dart';
 import '../../core/widgets/hamsa_app_bar.dart';
 import '../../../domain/core/entities/entities.dart';
+import 'package:share_plus/share_plus.dart';
 
 class HamsaCampaignDetailPage extends StatelessWidget {
   final HamsaCampaign campaign;
+  final FirebaseDynamicLinks dynamicLinks = FirebaseDynamicLinks.instance;
 
   static MaterialPageRoute route(HamsaCampaign campaign) {
     return MaterialPageRoute(
@@ -13,8 +16,7 @@ class HamsaCampaignDetailPage extends StatelessWidget {
     );
   }
 
-  const HamsaCampaignDetailPage({Key? key, required this.campaign})
-      : super(key: key);
+  HamsaCampaignDetailPage({Key? key, required this.campaign}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -25,9 +27,23 @@ class HamsaCampaignDetailPage extends StatelessWidget {
         appBarText: Text('Fundraiser'),
         actionButton: IconButton(
           icon: Icon(Icons.share_rounded),
-          onPressed: () {},
+          onPressed: _handleSharing,
         ),
       ),
     );
+  }
+
+  void _handleSharing() async {
+    final parameters = DynamicLinkParameters(
+      uriPrefix: 'https://lomi.page.link',
+      link: Uri.parse('https://hamsalomi.com/campaign/${campaign.id}'),
+      androidParameters: AndroidParameters(
+        packageName: 'org.hamsa.lomi',
+      ),
+    );
+
+    final uri = await dynamicLinks.buildLink(parameters);
+
+    Share.share(uri.toString());
   }
 }
