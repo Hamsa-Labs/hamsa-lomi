10290f802bfdf265339c10ac3b3b1a7a2058b843
eliasn <nebiyuelias1@gmail.com>
2022-02-19 11:00:10 +0300

    Rename donation creation use case

--- hamsa_lomi/lib/domain/donation_creation/use_cases/save_hamsa_campain_use_case.dart
+++ hamsa_lomi/lib/domain/donation_creation/use_cases/save_hamsa_campain_use_case.dart
@@ -10,13 +10,13 @@ import '../entities/create_hamsa_campaign.dart';
 import '../repositories/repositories.dart';
 
 @injectable
-class CreateHamsaCampaignUseCase
+class SaveHamsaCampaignUseCase
     extends BaseUseCase<HamsaCampaign, CreateHamsaCampaign> {
   final DonationCreationRepository _repository;
 
-  CreateHamsaCampaignUseCase(this._repository);
+  SaveHamsaCampaignUseCase(this._repository);
   @override
   Future<Either<Failure, HamsaCampaign>> call(CreateHamsaCampaign param) {
-    return _repository.createHamsaCampaign(param);
+    return _repository.saveHamsaCampaign(param);
   }
 }
